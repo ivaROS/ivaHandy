4870c0fcaf3695a70b42a15ccff7d0b4b716812e
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2023-03-29 13:27:30 -0700

    Update control, description, and gazebo packages for rviz/moveit control (#6)
    * wip: modify gazebo controllers for updated descriptions
    
    * Add successful gazebo run with trajectory controller
    
    * Update position control for gazebo
    
    * Remove unncessary torque controller and rename launch files for clarity
    
    * Remove most scripts
    
    We only need a single script to handle disabling torque on the motors
    (if necessary). The joint state script should be a state publisher node
    that remaps topics instead. We also try to make the finalarm_control the
    canonical source for scripts.
    
    * Add pre-commit
    
    * Refactor launch files to reuse more components
    
    * Update finalarm_description to use xacro
    
    * Update cmake file to generate urdf files
    
    * Update simplfied description for successful gazebo run
    
    * Remove extra files and update naming convention of launch files
    
    * Fix display launch files
    
    * Add testing of the gazebo related files
    
    * Fix simplified robot gazebo launch and moveit
    
    * Bump version to 2.0.0 for backwards incompatibility
    
    * fix: remove non-existing index from controller spawn
    
    * fix: reported issues with yaml module
    
    ---------
    
    Co-authored-by: Anthony Miyaguchi <amiyaguchi@gmail.com>

--- ros/finalarm_control/script/disable_torque_handy.py
+++ ros/finalarm_control/script/disable_torque_handy.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 
-import sys
 import rospy
-from dynamixel_controllers.srv import *
-from dynamixel_msgs.msg import *
+from dynamixel_controllers.srv import TorqueEnable
+from argparse import ArgumentParser
 
 # Enable/disable motor torque
-def set_torque_enable(joint, enable):
-    torque_enable_srv = "finalarm_position_controller_{}/torque_enable".format(joint)
+def set_torque_enable(model_name, joint, enable):
+    torque_enable_srv = "{}_position_controller_{}/torque_enable".format(model_name, joint)
     rospy.wait_for_service(torque_enable_srv)
     try:
         torque_enable_req = rospy.ServiceProxy(torque_enable_srv, TorqueEnable)
@@ -16,17 +15,24 @@ def set_torque_enable(joint, enable):
     except rospy.ServiceException as e:
         print("(Joint: {}) Torque Enable service call failed: {}".format(joint, enable))
 
-def usage():
-    return "{}".format(sys.argv[0])
+def parse_args():
+    parser = ArgumentParser(description='Disable torque for all joints of Handy')
+    parser.add_argument('--num_joints', type=int, default=9, help='Number of joints of Handy')
+    parser.add_argument(
+        "--model_name", type=str, default="finalarm", help="Name of the robot model"
+    )
+    args, _ = parser.parse_known_args()
+    return args
 
-def main(num_joints):
-    joint_list = [i+1 for i in range(num_joints)]
+def main():
+    args = parse_args()
+    joint_list = [i+1 for i in range(args.num_joints)]
     rospy.init_node('disable_torque_handy')
 
     for joint in joint_list:
         # Disable 'torque enable' of current joint/motor
-        set_torque_enable(joint, False)
+        set_torque_enable(args.model_name, joint, False)
 
 if __name__ == "__main__":
-    main(num_joints=9)
+    main()
     rospy.sleep(0.5)
--- ros/finalarm_control/script/joint_state_publisher.py
+++ ros/finalarm_control/script/joint_state_publisher.py
@@ -5,11 +5,12 @@ import rospy
 from sensor_msgs.msg import JointState as JointStateMoveIt
 from dynamixel_msgs.msg import JointState as JointStateDynamixel
 from functools import partial
+from argparse import ArgumentParser
 
 
 class JointStatePublisher:
-    def __init__(self, num_joints=9, rate=50):
-        rospy.init_node("finalarm_joint_state_publisher")
+    def __init__(self, num_joints=9, rate=50, model_name="finalarm"):
+        rospy.init_node("{}_joint_state_publisher".format(model_name))
 
         # a rate of 50 is 20Hz
         r = rospy.Rate(rate)
@@ -23,10 +24,14 @@ class JointStatePublisher:
         # Start controller state subscribers
         for idx in range(self.num_joints):
             topic = "/finalarm_position_controller_{}/state".format(idx + 1)
-            rospy.Subscriber(topic, JointStateDynamixel, partial(self.controller_state_handler, idx))
+            rospy.Subscriber(
+                topic, JointStateDynamixel, partial(self.controller_state_handler, idx)
+            )
 
         # Start publisher
-        self.joint_states_pub = rospy.Publisher("/joint_states", JointStateMoveIt, queue_size=100)
+        self.joint_states_pub = rospy.Publisher(
+            "/joint_states", JointStateMoveIt, queue_size=100
+        )
 
         rospy.loginfo("Publishing joint_state at {} Hz".format(rate))
 
@@ -46,6 +51,8 @@ class JointStatePublisher:
         msg.effort = []
 
         for state in self.joint_states:
+            if not state.name:
+                continue
             msg.name.append(state.name)
             msg.position.append(state.current_pos)
             msg.velocity.append(state.velocity)
@@ -56,9 +63,27 @@ class JointStatePublisher:
         self.joint_states_pub.publish(msg)
 
 
+def parse_args():
+    parser = ArgumentParser(description="Publish joint states for Handy")
+    parser.add_argument(
+        "--num_joints", type=int, default=9, help="Number of joints of Handy"
+    )
+    parser.add_argument(
+        "--rate", type=int, default=50, help="Publishing rate of joint states"
+    )
+    parser.add_argument(
+        "--model_name", type=str, default="finalarm", help="Name of the robot model"
+    )
+    args, _ = parser.parse_known_args()
+    return args
+
+
 if __name__ == "__main__":
+    args = parse_args()
     try:
-        s = JointStatePublisher(num_joints=9)
+        s = JointStatePublisher(
+            num_joints=args.num_joints, rate=args.rate, model_name=args.model_name
+        )
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
